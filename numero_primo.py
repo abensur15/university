7f3d47dc91603095b379cc3949da4011ba65311b
Enzo Abensur <135616484+abensur15@users.noreply.github.com>
2024-08-01 16:12:39 +0000

    Código atualizado em 01/08/2024 às 12:12

--- numero_primo.py
+++ numero_primo.py
@@ -1,13 +1,12 @@
-"""Funcao para verificar se algum numero eh primo"""
+"""Função para verificar se algum numero é primo"""
 def primo(num):
 	# Assume-se que 1 seja composto
 	if num == 1:
 		return False
 	else:
-		cont = 2
+		cont = 3
 		while cont < num:
-			# Se o numero for divisível por algum valor desse intervalo
-			# ele nao é primo
+			# Se o numero for divisível por algum valor desse intervalo, ele não é primo
 			if (num % cont) == 0:
 				return False
 
@@ -19,8 +18,7 @@ def primo(num):
 def main():
 	num = int(input())
 	
-	# Entrada limitada para apenas numeros positivos, incluindo 0
-	# Entrada termina em 0 (nao eh um caso de execucao)
+	# Entrada limitada para apenas números positivos
 	while num > 0:
 		if primo(num):
 			print("{}: primo".format(num))
