be92a2665e7d391f25eae1e2252e5e1201df4e57
Enzo Abensur <135616484+abensur15@users.noreply.github.com>
2024-08-03 15:47:09 -0400

    Update hello-world.py

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,5 +1,13 @@
-def hello_world():
-  print("Hello, World!")
+class HelloWorld:
+    def __init__(self, texto):
+        self.texto = texto
+    
+    def imprimir_texto(self):
+        print(self.texto)
+
+def main():
+    hello_world = HelloWorld("Hello, World!")
+    hello_world.imprimir_texto()
 
 if __name__ == '__main__':
-  hello_world()
+    main()
